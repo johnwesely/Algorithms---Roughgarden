1851708b2173c494c9f928e797a57bda6c992bba
johnwesely <johnpwesely@gmail.com>
2022-01-21 10:01:46 -0700

    Add files via upload

--- Part-4/TSP/bit-sets.c
+++ Part-4/TSP/bit-sets.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <limits.h>
 #include "bit-sets.h"
 
 size_t set_bit_index(size_t n) {  // returns index of first set bit
@@ -22,6 +24,45 @@ size_t count_bits(size_t n) {    // returns number of set bits
     return ret;
 }
 
-size_t create_bit_set(size_t n) { // create bit sets for all permutations of 25 bits
+void append_bit_set_member(size_t n, linked_list_t* bga) {  // append index of bit set member to appropriate list based on number of set bits
+    node_t* node = malloc(sizeof(node_t));
+    size_t bga_index = count_bits(n);
+    node->index = n;
+    node->next = bga[bga_index].head;
+    bga[bga_index].head = node;
+}
 
+size_t create_bit_set(size_t n) { // create bit sets for all permutations of n bits, for sets with up to 32 members
+    size_t set_max = pow(2, n);
+    bit_set_t* bit_set = malloc(sizeof(bit_set_t));
+    bit_set->bit_group_arr = calloc(n + 1, sizeof(linked_list_t));
+    bit_set->bsm_arr = malloc(sizeof(bsm_t) * set_max);
+    bit_set->size = n;
+    
+    for (size_t i = 0; i < set_max; ++i) {
+        bit_set->bsm_arr[i].key = i;
+        bit_set->bsm_arr[i].value = UINT_MAX;
+        bit_set->bsm_arr[i].last_j = UINT_MAX;
+        append_bit_set_member(i, bit_set->bit_group_arr);
+    }
+
+    return bit_set;
+}
+
+void destroy_linked_list(node_t* node) {
+    if (!node) return;
+
+    destroy_linked_list(node->next);
+    free(node);
+}
+
+void destroy_bit_set(bit_set_t* bit_set) {
+    free(bit_set->bsm_arr);
+
+    for (size_t i = 0; i < bit_set->size + 1; ++i) {
+        destroy_linked_list(bit_set->bit_group_arr[i].head);
+    }
+
+    free(bit_set->bit_group_arr);
+    free(bit_set);
 }
--- Part-4/TSP/bit-sets.h
+++ Part-4/TSP/bit-sets.h
@@ -11,13 +11,24 @@ typedef struct __bit_set_member {
     size_t last_j;    // id of last visted vertex 
 } bsm_t;
 
+typedef struct __linked_list_node {
+    size_t  index;      // index of bit set member
+    node_t* next;       // pointer to next member in list
+} node_t;
+
+typedef struct __linked_list_head {
+    node_t* head;
+} linked_list_t;
+
 typedef struct __bit_set {
-    size_t**  bit_group_arr;   // multi-dimensional array of bit_sets, first index is number of on bits in number
-    bsm_t*    bsm_arr;         // array of bit set members, index is key and numerical representation of bit set
+    size_t          size;            // size of bit_set
+    linked_list_t*  bit_group_arr;   // array of bit set member lists, index is number of on bits in numberical representation 
+    bsm_t*          bsm_arr;         // array of bit set members, index is key and numerical representation of bit set
 } bit_set_t;
 
 size_t set_bit_index(size_t n);  // returns index of first set bit
 size_t count_bits(size_t n);     // returns number of set bits
 size_t create_bit_set(size_t n); // create bit sets for all permutations of 25 bits
+void destroy_bit_set(bit_set_t* bit_set); // free bit set
 
 #endif
